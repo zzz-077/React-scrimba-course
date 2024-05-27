15f6b441f87bcab043dcdca2d66a40701d83b723
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-05-28 00:50:07 +0400

    day3: classname & exporting component files

---
+++ Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
--- index.js
+++ index.js
@@ -227,3 +227,37 @@ function Page() {
 
 ReactDOM.render(<Page />, document.getElementById("root"));
 */
+//////////////////////////////
+import Header from "./Header";
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
+}
+function Main() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+ReactDOM.render(<Page />, document.getElementById("root"));
